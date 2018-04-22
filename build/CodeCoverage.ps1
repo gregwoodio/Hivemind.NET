5b780d284861ae2eefc412ad6e57d48de5d5848a
Greg Wood <gregoryadamwood@gmail.com>
2018-04-22 09:43:17 -0400

    Updates code coverage script.

--- build/CodeCoverage.ps1
+++ build/CodeCoverage.ps1
@@ -5,6 +5,6 @@ if (!(Test-Path -Path $CoverageDir)) {
     New-Item -ItemType Directory -Path $CoverageDir
 }
 
-& 'D:\BuildTools\OpenCover.4.6.519\tools\OpenCover.Console.exe' -register:user -target:"D:\BuildTools\NUnit.org\nunit-console\nunit3-console.exe" -output:$CoverageDir\OpenCoverOutput.xml -targetargs:"D:\Jenkins\workspace\Hivemind\Hivemind.Tests\Hivemind.Tests.csproj" -mergebyhash -skipautoprops
+& 'D:\BuildTools\OpenCover.4.6.519\tools\OpenCover.Console.exe' -register:user -target:"D:\BuildTools\NUnit.org\nunit-console\nunit3-console.exe" -output:$CoverageDir\OpenCoverOutput.xml -targetargs:"D:\Jenkins\workspace\Hivemind\Hivemind.Tests\Hivemind.Tests.csproj" -mergebyhash -skipautoprops -filter:"+[*]* -[Moq*]*"
 
 & D:\BuildTools\ReportGenerator.3.1.2\tools\ReportGenerator.exe -reports:$CoverageDir\OpenCoverOutput.xml -targetdir:$CoverageDir\GeneratedReport
