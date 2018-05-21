802dbf2f6f081ddfa1d34e65ca007d74756c7f74
Greg Wood <gregoryadamwood@gmail.com>
2018-05-21 14:35:12 -0400

    Moves Hivemind.Core to myget.

--- Hivemind.Api.Core/Contracts/TerritoryIncomeReport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hivemind.Contracts
-{
-    public class TerritoryIncomeReport
-    {
-        public string TerritoryName { get; set; }
-        public string Description { get; set; }
-        public int Income { get; set; }
-    }
-}
